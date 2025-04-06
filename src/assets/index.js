6b336270cebdda532d3a26b804fd1579d99dd7e0
Huzaifakhalil <huzaifa.hado@gmail.com>
2025-04-06 14:56:36 +0500

    new updated

--- src/assets/index.js
+++ src/assets/index.js
@@ -34,7 +34,7 @@ import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
 
-import Dentalai from ".DentalAI.jpg";
+import Dentalai from "./DentalAI.jpg";
 import jasmin from './JasminPortal.jpg';
 import Reservation from "./Reservation.png";
 
